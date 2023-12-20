1f269f1503ef3121f238b80cad8928c80b579477
Milton Sobrinho <milton.sobrinho@mpba.mp.br>
2023-12-20 19:23:27 -0300

    Adição das páginas para o modelo e sobre.

---
+++ pages/sobre.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title('Sobre')
+
+
+st.header('Trabalho da disciplina Machine Learning Hands On', divider='grey')
+st.subheader('Especialização em Data Science & Analytics Senai/CIMATEC')
+
+st.write('\n\n\n')
+
+st.markdown(
+"""
+Equipe:
+- A
+- B
+- C
+"""
+)
