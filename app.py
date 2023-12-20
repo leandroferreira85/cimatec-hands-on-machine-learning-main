04642951316b55d5d7709b5890f1815688c513dc
Milton Sobrinho <milton.sobrinho@mpba.mp.br>
2023-12-20 19:40:44 -0300

    Adição da configuração do nome das páginas.

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st 
-
+from st_pages import Page, Section, add_page_title, show_pages
 
 st.header('🤖 CIMATEC - Hands On Machine Learning')
+
+
+
+show_pages([
+    #Section(name="Section1", icon=":books:"),
+    Page("pages/assistente-pessoal.py", "Assistente Pessoal", ":speech_balloon:"),
+    Page("pages/modelo.py", "Modelo", ":computer:"),
+    Page("pages/sobre.py", "Sobre", ":briefcase:") #:tophat:
+])
